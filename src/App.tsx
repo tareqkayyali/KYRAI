0576ba5fc782dbdef98ff805351bb48ebc1d96e2
tareqkayyali <tareq.kayyali@gmail.com>
2026-02-20 12:15:44 +0300

    Create src/App.tsx with main application component for KYRAI showcase

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>KYRAI Showcase</h1>
+            <p>Welcome to the KYRAI application!</p>
+        </div>
+    );
+};
+
+export default App;
