4dde4a44ce0c9c7524bffe30efde8241b4198f6f
tareqkayyali <tareq.kayyali@gmail.com>
2026-02-20 12:15:33 +0300

    Create Features component in Features.tsx file

---
+++ src/components/Features.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Features: React.FC = () => {
+    return (
+        <section className="features">
+            <h2>Features</h2>
+            <ul>
+                <li>Feature 1: Description of feature 1</li>
+                <li>Feature 2: Description of feature 2</li>
+                <li>Feature 3: Description of feature 3</li>
+                <li>Feature 4: Description of feature 4</li>
+            </ul>
+        </section>
+    );
+};
+
+export default Features;
