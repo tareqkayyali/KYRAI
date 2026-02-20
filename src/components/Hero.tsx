ff89510dfdd4a5e2f098dadcf982b4197d034f92
tareqkayyali <tareq.kayyali@gmail.com>
2026-02-20 12:15:39 +0300

    Add Hero component for the showcase

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+    return (
+        <div className="hero">
+            <h1>Welcome to KYRAI!</h1>
+            <p>Your showcase starts here.</p>
+            <button>Get Started</button>
+        </div>
+    );
+};
+
+export default Hero;
